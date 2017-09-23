de4d380e88ff6376da6736d62167aef99554d925
gpoudel <gaurab.poudel@gmail.com>
2017-09-23 12:36:27 +0200

    Added the map building code and data

---
+++ civ6Wonders.R
@@ -0,0 +1,29 @@
+
+#Clear the workspace - remove all variables
+rm(list = ls())
+gc()
+
+
+library('leaflet')
+library('htmlwidgets')
+
+wonders <- read.csv("civ6Wonders.csv", sep = ";", header = T)
+
+wonders$img_qry <- paste0("https://www.google.nl/search?tbm=isch&q=",gsub(" ","+",wonders$wonder))
+
+
+civ6_map <- leaflet(wonders) %>% 
+  addProviderTiles(providers$Esri.NatGeoWorldMap) %>% 
+  addCircleMarkers(fillOpacity = 1, 
+                         label = wonders$wonder, 
+                         color = ~ifelse(type == "m", "blue", "green"), 
+                         group = ~ifelse(type == "m", "Man-made", "Natural"),
+                         popup =  paste0("<a href='" ,wonders$img_qry, "' target='_blank'><img src = ", gsub(" ","",wonders$wonder), ".png> </a>")                         
+                   )%>% 
+  addLayersControl(
+    overlayGroups = c("Natural","Man-made"), 
+    options = layersControlOptions(collapsed = FALSE))
+
+
+
+saveWidget(widget=civ6_map, file="civ6.html")
